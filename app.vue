b7b233004ebccdeab5ca274e034e73ffaf57f084
Blake Arnold <blake@blakearnold.me>
2025-05-29 13:45:55 -0400

    :beers: added tailwind, daisyui, icons (#49)

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline text-center">
+      Hello World!
+    </h1>
+    <button class="btn btn-active btn-primary">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
