3424018f208ab370ab8981501c00e1789a5d2535
Blake Arnold <barnold@blakearnold.me>
2025-05-31 11:16:56 -0400

    :beers: login with github

---
+++ components/auth-button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign In
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github-filled" />
+  </button>
+</template>
