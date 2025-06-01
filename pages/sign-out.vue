ca8703543eaf2d6068415c931979bc68db504a29
Blake Arnold <barnold@blakearnold.me>
2025-05-31 21:13:48 -0400

    :beers: added login / logout

---
+++ pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card container bg-base-300 mx-auto min-h-72 mt-4 text-center flex flex-col items-center justify-center">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
