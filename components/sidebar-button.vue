97eb29d49842755b064c4d69c55e6bc530515dea
Blake Arnold <blake@blakearnold.me>
2025-06-02 21:52:04 -0400

    :beers: created sidebar (#59)

---
+++ components/sidebar-button.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel?: boolean;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <div
+    class="tooltip-right flex flex-col"
+    :data-tip="props.label"
+    :class="{ tooltip: !showLabel }"
+  >
+    <NuxtLink
+      :to="props.href"
+      :class="{ 'btn-secondary': route.path === props.href, 'justify-center': !showLabel, 'justify-start': showLabel }"
+      class="btn btn-primary"
+    >
+      <Icon :name="props.icon" size="24" />
+      <Transition name="grow">
+        <span v-if="showLabel">{{ props.label }}</span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.1s;
+}
+.grow-leave-active {
+  animation: grow 0.1s reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
