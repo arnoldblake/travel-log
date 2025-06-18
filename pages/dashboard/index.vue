c48cb75b3e5d3326ea1346db3a5c6478dc3c8d3a
Blake Arnold <barnold@blakearnold.me>
2025-06-18 09:43:23 -0400

    :beers: added dashboard content

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl">
+      Locations
+    </h1>
+    <div class="flex flex-col gap-2 mt-4">
+      <p>Add a location to get started.</p>
+      <NuxtLink to="/dashboard/add" class="btn btn-primary w-40">
+        Add Location
+        <Icon name="tabler:circle-plus-filled" size="24" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
