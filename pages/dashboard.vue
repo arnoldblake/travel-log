cbf37874b46fe6def15c660e9808c9119c5081b6
Blake Arnold <blake@blakearnold.me>
2025-06-21 18:12:34 -0400

    :beers: added map lib and map to dashboard (#65)

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -75,8 +75,9 @@ onMounted(() => {
         />
       </div>
     </div>
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
       <NuxtPage />
+      <AppMap class="flex-1" />
     </div>
   </div>
 </template>
